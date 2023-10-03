75e824934c263ea92b446099b699fd550eca449c
aura9k1 <rjoshpaddy@gmail.com>
2023-10-03 23:38:47 +0200

    refactored directory structure, updated card style

---
+++ public/assets/js/main.js
@@ -0,0 +1,48 @@
+let data;
+
+function render_view() {
+    data = JSON.parse(document.getElementById("data").innerHTML);
+
+    update_title();
+    update_bg();
+    update_services();
+    tick();
+
+    document.getElementById("body").classList.add("show");
+}
+
+function update_bg(){
+    document.getElementById("body").style.backgroundImage = `url("${data.bgImage}")`;
+}
+
+function update_title() {
+    document.title = `${data.owner}'s Home`;
+    document.getElementById("title").textContent = `${data.owner}'s Home`;
+    document.getElementById("title").title = `${data.host}`;
+}
+
+function update_services() {
+    document.getElementById("services").innerHTML = "";
+    for (var i = 0; i < data.services.length; i++) {
+        document.getElementById("services").innerHTML += `
+        <div class="card" onclick="window.open('${data.services[i].url}')">
+            <h3>${data.services[i].name}</h3>
+        </div>`
+    }
+}
+
+function tick(){
+    let ts = new Date(Date.now());
+    let days = ["Sun","Mon","Tues","Wed","Thu","Fri","Sat"];
+    let months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+
+    document.getElementById("timestamp").innerHTML = `${days[ts.getDay()]} ${ts.getDate()} ${months[ts.getMonth()]} ${ts.getFullYear()}<br>
+    <span class="min">${pad_hm(ts.getHours())}:${pad_hm(ts.getMinutes())}</span>
+    `
+}
+
+function pad_hm(i){
+    if (i < 10) return "0" + i;
+    return i;
+}
+//
